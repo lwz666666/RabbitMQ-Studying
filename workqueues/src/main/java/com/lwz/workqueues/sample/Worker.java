6371517aad11af6891f58f6cb3c1226089f8f69b
lwz <1149687979@qq.com>
2021-05-21 14:57:41 +0800

    第二篇:消费者代码

---
+++ workqueues/src/main/java/com/lwz/workqueues/sample/Worker.java
@@ -0,0 +1,55 @@
+package com.lwz.workqueues.sample;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+/**
+ * 基于工作队列消费者
+ *
+ * @author lwz
+ */
+public class Worker {
+
+    private static final String QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("116.62.179.12");
+        factory.setUsername("root");
+        factory.setPassword("rabbitmq123456");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        // 设置每次只消费一个消息
+        channel.basicQos(1);
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+            try {
+                doWork(message);
+            } finally {
+                System.out.println(" [x] Done");
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> {
+        });
+
+    }
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+}
